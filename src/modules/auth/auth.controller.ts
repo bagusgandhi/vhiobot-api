d745ce042e2335213e401b0c2f446ce74400e5b3
bagusgandhi <bagusgandhi4@gmail.com>
2024-03-31 05:14:18 +0700

    feat: auth for admin user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,12 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInCustomerDto } from './dto/signin-customer.dto';
+import { SignInAdminDto } from './dto/signin-admin.dto';
 import { ApiKeyGuard } from './guard/apikey.guard';
 import { Public } from 'src/decorators/public.decorator';
+import { SignInCustomerDto } from './dto/signin-customer.dto copy';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from './enum/roles.enum';
+import { SignUpAdminDto } from './dto/signup-admin.dto copy';
 
 @Controller('auth')
 export class AuthController {
@@ -14,8 +18,18 @@ export class AuthController {
     @Public()
     @Post('/signin')
     async signIn(@Body() signInCustomerDto: SignInCustomerDto) {
+        return await this.authService.signInCustomer(signInCustomerDto);
+    }
 
-        return this.authService.signInCustomer(signInCustomerDto);
+    @Post('/admin/signin')
+    async adminSignIn(@Body() signInAdminDto: SignInAdminDto){
+        return await this.authService.signInAdmin(signInAdminDto);
+    }
+
+    @Roles(Role.Administrator)
+    @Post('/admin/create')
+    async createAdmin(@Body() signUpAdminDto: SignUpAdminDto){
+        return await this.authService.signUpAdmin(signUpAdminDto);
     }
 
 
