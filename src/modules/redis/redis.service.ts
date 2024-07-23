ed459c4b7b9786cad6acd3f3e2c5943ec18ec752
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-07-23 14:40:05 +0700

    feat: add redis service

---
+++ src/modules/redis/redis.service.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { RedisClient } from './redis.provider';
+
+@Injectable()
+export class RedisService {
+  public constructor(
+    @Inject('REDIS_CLIENT')
+    private readonly client: RedisClient,
+  ) {}
+
+  async set(key: string, value: string, expirationSeconds: number) {
+    await this.client.set(key, value, 'EX', expirationSeconds);
+  }
+
+  async get(key: string): Promise<string | null> {
+    return await this.client.get(key);
+  }
+
+  async sadd(key: string, uuid:string){
+    return await this.client.sadd(key, uuid);
+  }
+
+  async scard(key: string){
+    return await this.client.scard(key);
+  }
+}
