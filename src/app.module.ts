c0dc814360f78dc52397390059a6ffb36a8b29ed
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-07-23 14:33:18 +0700

    feat: import module, databse, auth, user, dialogflow, socket, intent, analytics module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ChatModule } from './modules/chat/chat.module';
-import { DatabaseModule } from './config/database/database.module';
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/guard/jwt.guard';
 import { RolesGuard } from './modules/auth/guard/roles.guard';
 import { DialogflowModule } from './modules/dialogflow/dialogflow.module';
 import { SocketModule } from './modules/socket/socket.module';
-
+import { IntentModule } from './modules/intent/intent.module';
+import { AnalyticsModule } from './modules/analytics/analytics.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, ChatModule, UserModule, DialogflowModule, SocketModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    ChatModule,
+    UserModule,
+    DialogflowModule,
+    SocketModule,
+    IntentModule,
+    AnalyticsModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
-      useClass: JwtAuthGuard
+      useClass: JwtAuthGuard,
     },
     {
       provide: APP_GUARD,
-      useClass: RolesGuard
+      useClass: RolesGuard,
     },
   ],
 })
-export class AppModule { }
+export class AppModule {}
