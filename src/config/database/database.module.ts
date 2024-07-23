5948fa274552dd67372603e0c454aef46b772500
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-07-23 14:35:10 +0700

    feat: remove database and typeorm module for move folder

--- src/config/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import typeOrmConfig from './typeorm.config';
-
-@Module({
-    imports: [TypeOrmModule.forRoot(typeOrmConfig)]
-})
-
-export class DatabaseModule {}
