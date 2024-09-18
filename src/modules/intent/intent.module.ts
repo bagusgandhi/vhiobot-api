b63ccd30707f6392b1679c0d6000b2c4bb6217aa
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-09-18 23:46:32 +0700

    feat: import intent context model

--- src/modules/intent/intent.module.ts
+++ src/modules/intent/intent.module.ts
@@ -5,10 +5,11 @@ import { IntentService } from './intent.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Intent } from './entites/intent.entity';
 import { IntentRepository } from './repositories/intent.repositories';
+import { IntentContext } from './entites/intent-context.entity';
 
 @Module({
   imports : [
-    TypeOrmModule.forFeature([Intent]),
+    TypeOrmModule.forFeature([Intent, IntentContext]),
     DialogflowModule
   ],
   controllers: [IntentController],
