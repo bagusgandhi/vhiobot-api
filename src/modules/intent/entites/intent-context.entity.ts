fbf0fd05efb0beb507b1664f9b2ff80bb5038971
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-09-18 23:47:27 +0700

    feat: create intent context model

---
+++ src/modules/intent/entites/intent-context.entity.ts
@@ -0,0 +1,27 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('intent_context')
+export class IntentContext extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column()
+  title: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ nullable: true, name: 'deleted_at', type: 'timestamptz' })
+  deletedAt: Date;
+}
