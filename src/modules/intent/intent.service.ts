8fdabe09c3b3227cb01b1df27f095b68fac964d9
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-09-18 23:47:07 +0700

    feat: add bulk create intent context data

--- src/modules/intent/intent.service.ts
+++ src/modules/intent/intent.service.ts
@@ -10,6 +10,9 @@ import { IntentRepository } from './repositories/intent.repositories';
 import { CreateIntentDto } from './dto/create-intent.dto';
 import { GetAllIntentDto } from './dto/get-all-intent.dto';
 import { UpdateIntentDto } from './dto/update-intent.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IntentContext } from './entites/intent-context.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class IntentService {
@@ -17,6 +20,8 @@ export class IntentService {
   constructor(
     private readonly dialogflowService: DialogflowService,
     private readonly intentRepository: IntentRepository,
+    @InjectRepository(IntentContext)
+    private readonly intentContextRepository: Repository<IntentContext>,
   ) {}
 
   async getAllIntent(query: GetAllIntentDto) {
@@ -43,17 +48,18 @@ export class IntentService {
   }
 
   async createIntent(createIntentDto: CreateIntentDto) {
-    const { trainingPhrasesParts, messageTexts, displayName } = createIntentDto;
+    const { trainingPhrasesParts, messageTexts, displayName, inputContext, outputContext } = createIntentDto;
 
     // run query and start transaction
     const queryRunner =
       this.intentRepository.manager.connection.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
+    let response: any;
 
     try {
       // request to dialogflow create intent
-      const response =
+      response =
         await this.dialogflowService.createIntent(createIntentDto);
 
       // Save intent to database
@@ -63,13 +69,29 @@ export class IntentService {
       newIntent.responseTexts = messageTexts;
       newIntent.dfIntentId = response.name; // dialogflow intentId
 
+      let uniqueContext: string[] | null | undefined = [];
+
+      if(inputContext?.length){
+        newIntent.input_context = inputContext;
+        uniqueContext = Array.from(new Set(inputContext))
+      }
+
+      if(outputContext?.length){
+        newIntent.output_context = outputContext;
+        uniqueContext = Array.from(new Set([...uniqueContext,...outputContext]))
+      }
+
       const result = await queryRunner.manager.save(Intent, newIntent);
+
+      console.log("uniqueContext", uniqueContext)
+      uniqueContext.length && await this.bulkInsertIntentContext(uniqueContext)
 
       await queryRunner.commitTransaction();
       return result;
     } catch (error) {
       // rollback data when something wrong
       await queryRunner.rollbackTransaction();
+      response.name && await this.dialogflowService.deleteIntent(response.name)
 
       this.logger.error(error);
       throw new HttpException(error.message, 500);
@@ -79,7 +101,7 @@ export class IntentService {
   }
 
   async updateIntent(uuid: string, updateIntentDto: UpdateIntentDto) {
-    const { trainingPhrasesParts, messageTexts, displayName } = updateIntentDto;
+    const { trainingPhrasesParts, messageTexts, displayName, inputContext, outputContext } = updateIntentDto;
 
     // Run query and start transaction
     const queryRunner =
@@ -107,7 +129,21 @@ export class IntentService {
       existingIntent.trainingPhrases = trainingPhrasesParts;
       existingIntent.responseTexts = messageTexts;
 
+      let uniqueContext: string[] | null | undefined = [];
+
+      if(inputContext?.length){
+        existingIntent.input_context = inputContext;
+        uniqueContext = Array.from(new Set(inputContext))
+      }
+
+      if(outputContext?.length){
+        existingIntent.output_context = outputContext;
+        uniqueContext = Array.from(new Set([...uniqueContext,...outputContext]))
+      }
+
       const result = await queryRunner.manager.save(Intent, existingIntent);
+
+      uniqueContext.length && await this.bulkInsertIntentContext(uniqueContext)
 
       await queryRunner.commitTransaction();
       return result;
@@ -141,6 +177,45 @@ export class IntentService {
       await queryRunner.manager.delete(Intent, { uuid });
 
       await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async getAllIntentContext() {
+    try {
+      const intentContext = await this.intentContextRepository.find();
+      return intentContext;
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(error.message, error.statusCode);
+    }
+  }
+
+  async bulkInsertIntentContext(data: string[]){
+    const queryRunner = this.intentContextRepository.manager.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+
+      const values = data?.map(title => ({ title }))
+
+      const result = await queryRunner.manager
+        .createQueryBuilder()
+        .insert()
+        .into(IntentContext)
+        .values(values)
+        .orIgnore()
+        .execute();
+
+      await queryRunner.commitTransaction();
+      
+      return result;
+      
     } catch (error) {
       await queryRunner.rollbackTransaction();
       throw error;
